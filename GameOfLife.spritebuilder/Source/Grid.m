ab8a65f086619ada8ac42726859f109a23eb56bd
Kendall Agbulos <Jarvis@Kendalls-MacBook-Pro.local>
2014-07-12 13:53:32 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -163,7 +163,7 @@ static const int GRID_COLUMNS = 10;
             else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4)
             {
                 currentCreature.isAlive = FALSE;
-                numAlive--;
+                //numAlive--;
             }
             
         }
